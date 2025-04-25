94c2a85b03e627b53a628ffe4a781a792585df68
alexx <aknight13@students.avc.edu>
2025-04-25 13:12:38 -0700

    Assigment Completed

--- main.hpp
+++ main.hpp
@@ -1,8 +1,40 @@
+#include <iostream>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+// Function declarations
+void getTwoValues(int &begin, int &end);
+int getNextPrime(int begin);
+int getPrevPrime(int end);
+bool isPrime(int num);
 
-// ******************************
-// Implement all your functions here
-// ******************************
+void getTwoValues(int &begin, int &end) {
+    do {
+        cout << "Enter two numbers: ";
+        cin >> begin >> end;
+    } while (begin >= end);
+}
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i < num; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int getNextPrime(int begin) {
+    int num = begin + 1;
+    while (true) {
+        if (isPrime(num)) return num;
+        num++;
+    }
+}
+
+int getPrevPrime(int end) {
+    int num = end - 1;
+    while (num > 1) {
+        if (isPrime(num)) return num;
+        num--;
+    }
+    return -1;
+}
